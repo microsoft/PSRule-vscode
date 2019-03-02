215e3ffedeb8a19cf11607f5cdb781dd7358e959
Bernie White <bernardawhite@hotmail.com>
2019-03-02 17:13:05 +1000

    Update dependencies (#1)

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,8 @@
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import fs = require("fs");
-import path = require("path");
+// import fs = require("fs");
+// import path = require("path");
 import vscode = require("vscode");
 
 // this method is called when your extension is activated
