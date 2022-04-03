42112c602badcce5a1da688821cc0a95a6c445dd
Bernie White <bewhite@microsoft.com>
2022-04-03 20:39:34 +1000

    Additional updates

--- src/test/suite/configuration.test.ts
+++ src/test/suite/configuration.test.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import * as assert from 'assert';
+import * as assert from 'assert/strict';
 import { configuration, OutputAs } from '../../configuration';
 
 suite('ConfigurationManager tests', () => {
     test('Defaults', () => {
         assert.strictEqual(configuration.get().executionNotProcessedWarning, false);
-        assert.strictEqual(configuration.get().outputAs, 'Summary');
+        assert.strictEqual<OutputAs>(configuration.get().outputAs, OutputAs.Summary);
     });
 });
