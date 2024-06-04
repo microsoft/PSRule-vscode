868f558248454f5eeffefc16333be93b33aa7d14
Bernie White <bewhite@microsoft.com>
2024-06-04 16:47:14 +1000

    Added server tooling #225 (#1434)

--- dotnet/PSRule.EditorServices/Program.cs
+++ dotnet/PSRule.EditorServices/Program.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.CommandLine;
 using System.Threading.Tasks;
 
@@ -8,11 +9,14 @@ namespace PSRule.EditorServices;
 
 static class Program
 {
-  /// <summary>
-  /// Entry point for CLI tool.
-  /// </summary>
-  static async Task<int> Main(string[] args)
-  {
-    return await ClientBuilder.New().InvokeAsync(args);
-  }
+    /// <summary>
+    /// Entry point for CLI tool.
+    /// </summary>
+    static async Task<int> Main(string[] args)
+    {
+        var user = System.Management.Automation.ModuleIntrinsics.GetPSModulePath(System.Management.Automation.ModuleIntrinsics.PSModulePathScope.User);
+        System.Environment.SetEnvironmentVariable("PSModulePath", user, EnvironmentVariableTarget.Process);
+
+        return await ClientBuilder.New().InvokeAsync(args);
+    }
 }
